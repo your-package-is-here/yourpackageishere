b0b031e0849170c84c78edd9ecc0d78e2a27c1d5
river-ceanne <snvencer@gmail.com>
2019-06-21 14:31:46 -0700

    ... initial commit

---
+++ src/main/java/com/teamshort/rocks/YourPackageIsHere/YourPackageIsHereApplication.java
@@ -0,0 +1,13 @@
+package com.teamshort.rocks.YourPackageIsHere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourPackageIsHereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourPackageIsHereApplication.class, args);
+	}
+
+}
