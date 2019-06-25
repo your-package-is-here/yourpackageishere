d0554a3ac10ab9579e882210a9d74d172877bf28
river-ceanne <snvencer@gmail.com>
2019-06-25 09:28:05 -0700

    added tenant html

--- src/main/java/com/teamshort/rocks/YourPackageIsHere/TenantController.java
+++ src/main/java/com/teamshort/rocks/YourPackageIsHere/TenantController.java
@@ -2,6 +2,7 @@ package com.teamshort.rocks.YourPackageIsHere;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TenantController {
@@ -9,5 +10,9 @@ public class TenantController {
     @Autowired
     TenantRepository appUserRepository;
 
+    @GetMapping("/tenant/{type}")
+    public String getTenantPage() {
+        return "tenant";
+    }
 
 }
