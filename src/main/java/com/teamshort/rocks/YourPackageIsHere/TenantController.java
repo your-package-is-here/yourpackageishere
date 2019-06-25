e1b130ed2a78f7851d5a7dd5d6a875f4e7c6a019
Devon Hackley <devonhackley@gmail.com>
2019-06-25 10:34:19 -0700

    finished with basic layout and route for all tenants

--- src/main/java/com/teamshort/rocks/YourPackageIsHere/TenantController.java
+++ src/main/java/com/teamshort/rocks/YourPackageIsHere/TenantController.java
@@ -2,12 +2,28 @@ package com.teamshort.rocks.YourPackageIsHere;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class TenantController {
 
     @Autowired
-    TenantRepository appUserRepository;
+    TenantRepository tenantRepository;
 
+
+
+
+
+    @GetMapping("/tenant/all")
+    public String getAllTenants(Principal principal, Model model){
+        String p = principal == null ? "" : principal.getName();
+        Iterable<Tenant> tenants = tenantRepository.findAll();
+        model.addAttribute("principal", p);
+        model.addAttribute("tenants", tenants);
+        return "allTenants";
+    }
 
 }
