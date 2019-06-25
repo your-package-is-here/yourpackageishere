1e0d202a29063b8cce1e81ba28fb405ca5f1e60f
river-ceanne <snvencer@gmail.com>
2019-06-25 11:32:17 -0700

    tenantEdit frag made :D

--- src/main/java/com/teamshort/rocks/YourPackageIsHere/TenantController.java
+++ src/main/java/com/teamshort/rocks/YourPackageIsHere/TenantController.java
@@ -5,12 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.security.Principal;
+import java.text.ParseException;
 
 @Controller
 public class TenantController {
 
     @Autowired
     TenantRepository tenantRepository;
+
+    @Autowired
+    BuildingRepository buildingRepository;
 
     @GetMapping("/tenant/{id}")
     public String getTenantPage(@PathVariable String id, Model m) {
@@ -23,6 +31,15 @@ public class TenantController {
     @GetMapping("/tenant/add")
     public String getTenantAddPage() {
         return "tenant";
+    }
+
+    @PostMapping("/tenantcreate")
+    public RedirectView createUser(Principal p, String firstname, String lastname, String email, String aptnum, String phonenum) throws ParseException {
+        Building building = buildingRepository.findByUsername(p.getName());
+        Tenant tenant = new Tenant(firstname,lastname,email,aptnum,phonenum, building);
+        tenantRepository.save(tenant);
+
+        return new RedirectView("/");
     }
 
 }
