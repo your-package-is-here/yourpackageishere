eb57c0d90c50134b3690f24a36cc0b06db5d6b72
river-ceanne <snvencer@gmail.com>
2019-06-25 09:58:00 -0700

    added route for /tenant/{id} --> go to single tenant page

--- src/main/java/com/teamshort/rocks/YourPackageIsHere/TenantController.java
+++ src/main/java/com/teamshort/rocks/YourPackageIsHere/TenantController.java
@@ -2,16 +2,21 @@ package com.teamshort.rocks.YourPackageIsHere;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class TenantController {
 
     @Autowired
-    TenantRepository appUserRepository;
+    TenantRepository tenantRepository;
 
-    @GetMapping("/tenant/{type}")
-    public String getTenantPage() {
+    @GetMapping("/tenant/{id}")
+    public String getTenantPage(@PathVariable String id, Model m) {
+        long ID = Long.parseLong(id);
+        Tenant tenant = tenantRepository.findById(ID);
+        m.addAttribute("tenant", tenant);
         return "tenant";
     }
 
--- src/main/java/com/teamshort/rocks/YourPackageIsHere/TenantRepository.java
+++ src/main/java/com/teamshort/rocks/YourPackageIsHere/TenantRepository.java
@@ -3,4 +3,7 @@ package com.teamshort.rocks.YourPackageIsHere;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TenantRepository extends CrudRepository<Tenant, Long> {
+
+    Tenant findById(long id);
+
 }
