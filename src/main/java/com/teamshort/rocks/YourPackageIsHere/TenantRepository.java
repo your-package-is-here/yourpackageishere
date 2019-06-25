20b7a7a6468e91e335f6e2a2baf4251bc5fe0e5c
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-06-25 12:39:16 -0700

    added find tenants by firstname, lastname and aptnum

--- src/main/java/com/teamshort/rocks/YourPackageIsHere/TenantRepository.java
+++ src/main/java/com/teamshort/rocks/YourPackageIsHere/TenantRepository.java
@@ -2,5 +2,9 @@ package com.teamshort.rocks.YourPackageIsHere;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TenantRepository extends CrudRepository<Tenant, Long> {
+    List<Tenant> findByFirstnameIgnoreCaseContainingAndLastnameIgnoreCaseContaining(String firstname, String lastname);
+    List<Tenant> findByAptnumIgnoreCaseContaining(String aptnum);
 }
