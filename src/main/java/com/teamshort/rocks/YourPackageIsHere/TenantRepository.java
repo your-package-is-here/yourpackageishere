dfc1b34c6cc724e98e6acf38fed18dd8dfa5de50
John Winters <jhnwin@amazon.com>
2019-06-26 14:36:41 -0700

    fixed a bug and fixed the route

--- src/main/java/com/teamshort/rocks/YourPackageIsHere/TenantRepository.java
+++ src/main/java/com/teamshort/rocks/YourPackageIsHere/TenantRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface TenantRepository extends CrudRepository<Tenant, Long> {
-    List<Tenant> findByFirstnameIgnoreCaseContainingAndLastnameIgnoreCaseContaining(String firstname, String lastname);
-    List<Tenant> findByAptnumIgnoreCaseContaining(String aptnum);
+    List<Tenant> findByFirstnameIgnoreCaseAndLastnameIgnoreCase(String firstname, String lastname);
+    List<Tenant> findByAptnumIgnoreCase(String aptnum);
     Tenant findById(long id);
     Tenant findByEmail(String email);
 }
