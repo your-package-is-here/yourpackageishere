26cf27ef5fb819f5b1552fad9004ddc120cc2bfe
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-06-27 16:21:07 -0700

    added building id to go with tenants

--- src/main/java/com/teamshort/rocks/YourPackageIsHere/TenantRepository.java
+++ src/main/java/com/teamshort/rocks/YourPackageIsHere/TenantRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface TenantRepository extends CrudRepository<Tenant, Long> {
-    List<Tenant> findByFirstnameIgnoreCaseAndLastnameIgnoreCase(String firstname, String lastname);
-    List<Tenant> findByAptnumIgnoreCase(String aptnum);
+    List<Tenant> findByFirstnameIgnoreCaseAndLastnameIgnoreCaseAndBuildingId(String firstname, String lastname, long id);
+    List<Tenant> findByAptnumIgnoreCaseAndBuildingId(String aptnum, long id);
     Tenant findById(long id);
     Tenant findByEmail(String email);
     List<Tenant> findByBuilding(Building building);
