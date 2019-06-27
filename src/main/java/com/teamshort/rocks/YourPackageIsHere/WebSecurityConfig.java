efcd9569a2b30791c33fdf2ea4f64ac1cd1e5035
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-06-27 15:49:43 -0700

    about us css fixed, assets folder added in webconfig

--- src/main/java/com/teamshort/rocks/YourPackageIsHere/WebSecurityConfig.java
+++ src/main/java/com/teamshort/rocks/YourPackageIsHere/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 // allow requests to all URLS that match the patterns even if not logged in
-                .antMatchers("/", "/aboutus","/login","/buildingcreate" ,"/register", "/*.css").permitAll()
+                .antMatchers("/", "/aboutus","/login","/buildingcreate" ,"/register", "/*.css", "/assets/*").permitAll()
                 // anything else, you must be logged in
                 .anyRequest().authenticated()
                 .and()
