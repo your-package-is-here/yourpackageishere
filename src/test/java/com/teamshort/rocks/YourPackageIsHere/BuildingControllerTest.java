d4b2da9c01b67e49056ef7b69b7ae7a8f66e4623
Devon Hackley <devonhackley@gmail.com>
2019-06-27 15:58:56 -0700

    adding more integration tests

--- src/test/java/com/teamshort/rocks/YourPackageIsHere/BuildingControllerTest.java
+++ src/test/java/com/teamshort/rocks/YourPackageIsHere/BuildingControllerTest.java
@@ -1,42 +1,45 @@
 package com.teamshort.rocks.YourPackageIsHere;
 
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 
-@RunWith(SpringRunner.class)
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 @AutoConfigureMockMvc
+@ContextConfiguration
 public class BuildingControllerTest {
 
     //  -----------  INTEGRATION TESTS FOR GET REQS  ----------------  //
-
-//    @GetMapping("/")
-//    public String getHomePage(Principal principal, Model model)
-//
-//@GetMapping("/login")
-//    public String getLoginPage(Principal principal, Model model)
-//
-//@GetMapping("/register")
-//    public String getRegisterPage(Principal principal, Model model)
-//
 //@GetMapping("/aboutus")
 //    public String getSignUpPage(Principal principal, Model model)
-//
-//@PostMapping("/buildingcreate")
-//    public RedirectView createUser(String username, String name, String streetaddress, String city, String state, String zip, String email, String password) throws ParseException
 //
 
     //  -----------  END-TO-END TESTS  ----------------  //
@@ -51,6 +54,27 @@ public class BuildingControllerTest {
     @Autowired
     BuildingRepository buildingRepository;
 
+    @Autowired
+    WebApplicationContext context;
+
+    private InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+        viewResolver.setPrefix("/templates/");
+        viewResolver.setSuffix("*.html");
+
+        return viewResolver;
+    }
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+
     //Private helper test function to initialize a Building instance
     private Building initialize(){
         return new Building("bloop","Bloop Building","1 Bloop Ave",
@@ -58,10 +82,35 @@ public class BuildingControllerTest {
 
     }
 
+    public static RequestPostProcessor testUser() throws Exception {
+        return user("bloop").password("bloop123").roles("ADMIN");
+    }
+
+    // This test will work on a page that DOES NOT require auth.
+    @Test
+    public void testingAPageThatDoesntRequireAuth() throws Exception {
+        mockMvc.perform(get("/")).andExpect(content().string(containsString("Home")));
+    }
+
+    // This route requires auth.
+    @WithMockUser
+    @Test
+    public void testGetSendEmail() throws Exception {
+        Building building = initialize();
+        mockMvc.perform(
+                get("/sendemail").with(testUser())).andExpect(content().string(containsString("Scan Package")));
+    }
+
+    @Test
+    public void testSendEmailHelper() throws Exception {
+        mockMvc.perform(
+                post("")
+        )
+
+    }
+
     @Test
     public void testBuildingCRUD() throws Exception {
-        Building building = initialize();
-
         mockMvc.perform(
                 post("/buildingcreate")
                         .param("username", "bloop")
@@ -77,7 +126,7 @@ public class BuildingControllerTest {
 
         Building buildingResCreate = buildingRepository.findByUsername("bloop");
 
-        assertEquals(building.name,buildingResCreate.getName()); // check Creation and Read data
+        assertEquals("Bloop Building",buildingResCreate.getName()); // check Creation and Read data
 
         //UPDATE instance' name
         buildingResCreate.setName("New Name");
@@ -94,4 +143,5 @@ public class BuildingControllerTest {
         assertNull(buildingRepository.findByUsername("bloop"));
 
     }
+
 }
